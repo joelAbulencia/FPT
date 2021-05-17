a867853cdc641bbcb05f948f902aee7422c6d70e
joelAbulencia <joel.a.p.abulencia@gmail.com>
2021-05-17 20:10:25 +0800

    Add files via upload

---
+++ FPTCodeExamBusiness/CityBusiness.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using FPTCodeExamDomain;
+using Newtonsoft.Json;
+
+namespace FPTCodeExamBusiness
+{
+    public class CityBusiness
+    {
+        public string RetrieveCityName(string url)
+        {
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
+            webrequest.Method = "GET";
+            webrequest.ContentType = "application/x-www-form-urlencoded";
+            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+            StreamReader responseStream = new StreamReader(webresponse.GetResponseStream(), System.Text.Encoding.GetEncoding("utf-8"));
+            string result = string.Empty;
+            result = responseStream.ReadToEnd();
+            webresponse.Close();
+            CityDetails cityDetails = JsonConvert.DeserializeObject<CityDetails>(result);
+            return cityDetails.city;
+        }
+    }
+}
